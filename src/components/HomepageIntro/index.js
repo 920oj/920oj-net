1467c030223f33794f55a22c0ff98b18fdb793a7
Junya Oiwa <0920oiwa@gmail.com>
2026-06-29 11:43:54 +0900

    feat: change toppage

--- src/components/HomepageIntro/index.js
+++ src/components/HomepageIntro/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
@@ -9,14 +8,14 @@ export default function HomepageIntro() {
         <h2>About Me</h2>
         <div className="row">
           <div className={clsx('col col--4')}>
-            <img src="/img/portrait.jpg"></img>
+            <img src="/img/portrait.jpg" alt="Profile"></img>
           </div>
           <div className={clsx('col col--8')}>
             <h3>おーじぇい</h3>
-            <p>エンジニア / ITコンサルタント</p>
+            <p>ソフトウェアエンジニア</p>
             <p>1999年9月生まれ。愛知県出身。現在は神奈川県横浜市在住。</p>
-            <p>大学在学に趣味でプログラミングを始め、フロントエンドからバックエンド、インフラ等幅広い領域の開発を経験。また、友人が立ち上げた会社にて、リードエンジニアとして設計やチームリーディングに携わる。現在は都内にてITコンサルタントとして勤務。</p>
-            <p>(Photos by <a href="https://twitter.com/kspace_trk" target="_blank">@kspace_trk</a>)</p>
+            <p>大学在学に趣味でプログラミングを始め、フロントエンドからバックエンド、インフラ等幅広い領域の開発を経験。また、友人が立ち上げた会社にて、リードエンジニアとして設計やチームリーディングに携わる。現在は都内にてソフトウェアエンジニアとして勤務。</p>
+            <p>(Photos by <a href="https://twitter.com/kspace_trk" target="_blank" rel="noopener noreferrer">@kspace_trk</a>)</p>
           </div>
         </div>
       </div>
